7e25ba66e467286dc0037752b06424272e806f46
kimeuiyeob <rladmlduq47@gmail.com>
2022-11-27 16:54:03 +0900

    설계수정

--- milestone/src/main/java/com/app/milestone/repository/ServiceRepository.java
+++ milestone/src/main/java/com/app/milestone/repository/ServiceRepository.java
@@ -1,7 +1,9 @@
 package com.app.milestone.repository;
 
+import com.app.milestone.entity.Service;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
 @Component
-public interface ServiceRepository {
+public interface ServiceRepository extends JpaRepository<Service, Long> {
 }
